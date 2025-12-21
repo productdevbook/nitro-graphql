4f8566d10c1aba0dbc23884dc478fdd338266c87
productdevbook <hi@productdevbook.com>
2025-12-21 21:16:17 +0300

    chore(changelog): update changelog generation logic and add configuration file

---
+++ changelogithub.config.ts
@@ -0,0 +1,19 @@
+export default {
+  types: {
+    feat: { title: '🚀 Features' },
+    fix: { title: '🐞 Bug Fixes' },
+    perf: { title: '🏎 Performance' },
+    refactor: { title: '💅 Refactors' },
+    docs: { title: '📖 Documentation' },
+    build: { title: '📦 Build' },
+    types: { title: '🌊 Types' },
+    chore: { title: '🏡 Chore' },
+    examples: { title: '🏀 Examples' },
+    test: { title: '✅ Tests' },
+    style: { title: '🎨 Styles' },
+    ci: { title: '🤖 CI' },
+  },
+  contributors: true,
+  capitalize: true,
+  group: true,
+}
